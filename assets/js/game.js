331a26117db6ec25224ce4d191e0b3d94b823686
Josh <josh.haddix598@gmail.com>
2022-06-07 13:38:41 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,46 +19,60 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
-    do{
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.").toUpperCase();
-
-        if(!(fightskip.includes(promptFight))){
-            window.alert("Please enter a valid response, 'FIGHT' or 'SKIP' (capitalization matters).")
-        }
-    } while(!(fightskip.includes(promptFight)) )
-
-    if(promptFight==="FIGHT"){
-        enemyHealth -= playerAttack;
-        if (enemyHealth <= 0) {
-            window.alert(`${enemyName} has been destroyed!`);
-        }
-        else {
-            window.alert(`${enemyName} takes the blow and has ${enemyHealth} left`) // I know what f literals are in python. I can't go back. i won't go back. you can't make me.
-        }
-        playerHealth -= enemyAttack;
-        if (playerHealth <= 0) {
-            window.alert(`${playerName} has been destroyed!`);
-        }
-        else {
-            window.alert(`${playerName} takes the blow and has ${playerHealth} left`);
-        }
-        console.log("enemy health: " + enemyHealth, "\nplayer health: " + playerHealth);
-    }
-    else{
-        if(window.confirm("Are you sure you want to back out? There is a 2 gold penalty.")){
-            window.alert(`${playerName} has chosen to skip this fight.`); // coward.
-            playerMoney-=2;
+    while(enemyHealth > 0 && playerHealth > 0){
+        do{
+            var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.").toUpperCase();
+    
+            if(!(fightskip.includes(promptFight))){
+                window.alert("Please enter a valid response, 'FIGHT' or 'SKIP' (capitalization matters).")
+            }
+        } while(!(fightskip.includes(promptFight)) )
+    
+        if(promptFight==="FIGHT"){
+            enemyHealth -= playerAttack;
+            if (enemyHealth <= 0) {
+                window.alert(`${enemyName} has been destroyed!`);
+                break;
+            }
+            else {
+                window.alert(`${enemyName} takes the blow and has ${enemyHealth} left`) // I know what f literals are in python. I can't go back. i won't go back. you can't make me.
+            }
+            playerHealth -= enemyAttack;
+            if (playerHealth <= 0) {
+                window.alert(`${playerName} has been destroyed!`);
+                break;
+            }
+            else {
+                window.alert(`${playerName} takes the blow and has ${playerHealth} left`);
+            }
+            console.log("enemy health: " + enemyHealth, "\nplayer health: " + playerHealth);
         }
         else{
-            fight();
+            if(window.confirm("Are you sure you want to back out? There is a 10 gold penalty.")){
+                window.alert(`${playerName} has chosen to skip this fight.`); // coward.
+                playerMoney-=10;
+                break;
+            }
+            else{
+                fight();
+            }
+            
         }
-        
-    }
+    }   
 
 };
 
+
 for(var i =0; i<enemies.length; i++){
-    fight(enemies[i]);
+    if(playerHealth > 0){
+        window.alert(`Welcome to Robot Gladiators!\nGet ready for Round ${i+1}!`);
+    }
+    else{
+        window.alert("Your robot has been destroyed!\nGAME OVER");
+        break;
+    }
+    var currentEnemy = enemies[i]
+    enemyHealth=50;
+    fight(currentEnemy);
 }
 // fight();
